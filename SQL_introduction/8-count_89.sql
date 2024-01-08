47f42363380f3e21035d45642a1122d382ccb17a
Shemaremy <remyshema20@gmail.com>
2024-01-08 15:15:45 +0200

    commit 392

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -5,12 +5,5 @@ CREATE TABLE IF NOT EXISTS first_table (
     name VARCHAR(256)
 );
 
--- Create records
-INSERT INTO first_table (id, name) VALUES (1, "A");
-INSERT INTO first_table (id, name) VALUES (89, "B");
-INSERT INTO first_table (id, name) VALUES (2, "C");
-INSERT INTO first_table (id, name) VALUES (89, "D");
-INSERT INTO first_table (id, name) VALUES (89, "E");
-INSERT INTO first_table (id, name) VALUES (3, "F");
-
+ 
 SELECT COUNT(*) FROM first_table WHERE id = 89;
