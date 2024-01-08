c157d5b37da67d53bbaf318dd338e607ee3528ac
Shemaremy <remyshema20@gmail.com>
2024-01-08 20:36:48 +0200

    commit 321

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,6 @@
 -- A script that lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id, cities.name AS city_name, states.name AS state_name
+
+SELECT cities.id, cities.name AS name, states.name AS name
 FROM cities
 JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
