2535c5ea4099dd1056f961f5905dfffaa2de81de
Shemaremy <remyshema20@gmail.com>
2024-01-08 15:25:39 +0200

    commit 293

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- A script that creates a table second_table in the database hbtn_0c_0
+CREATE table IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table VALUES (
+    id = 1, name = “John”, score = 10
+    id = 2, name = “Alex”, score = 3
+    id = 3, name = “Bob”, score = 14
+    id = 4, name = “George”, score = 8
+
+)
+
