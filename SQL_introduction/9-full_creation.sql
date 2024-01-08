8ae30a7412e78131a9b1fced1a75a88c33520098
Shemaremy <remyshema20@gmail.com>
2024-01-08 15:51:45 +0200

    commit 778

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,13 @@
--- A script that creates a table second_table in the database hbtn_0c_0
-DROP DATABASE IF EXISTS hbtn_test_db_9;
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_9;
-USE hbtn_test_db_9;
-CREATE table IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table (id, name, score) VALUES (1, “John”, 10);
-INSERT INTO second_table (id, name, score) VALUES(2, “Alex”, 3);
-INSERT INTO second_table (id, name, score) VALUES(3, “Bob”, 14);
-INSERT INTO second_table (id, name, score) VALUES(4, “George”, 8);
+-- Create second_table
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT PRIMARY KEY,
+    name VARCHAR(256),
+    score INT
+);
 
-
+-- Add records to second_table
+INSERT INTO second_table (id, name, score) VALUES
+    (1, 'John', 10),
+    (2, 'Alex', 3),
+    (3, 'Bob', 14),
+    (4, 'George', 8);
