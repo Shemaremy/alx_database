d030baa6264fe5008649f0d8b777ee5ae7556c23
Shemaremy <remyshema20@gmail.com>
2024-01-08 16:21:49 +0200

    commit 293

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,11 +1,11 @@
--- Delete user user_0d_1 
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
-
 -- Set the MySQL user and password
 SET @mysql_user = 'user_0d_1';
 SET @mysql_password = 'user_0d_1_pwd';
 
+-- Drop the user if it exists
+DROP USER IF EXISTS @mysql_user@'localhost';
+
 -- Create the user with all privileges
-CREATE USER IF NOT EXISTS @mysql_user@'localhost' IDENTIFIED BY @mysql_password;
+CREATE USER @mysql_user@'localhost' IDENTIFIED BY @mysql_password;
 GRANT ALL PRIVILEGES ON *.* TO @mysql_user@'localhost' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
