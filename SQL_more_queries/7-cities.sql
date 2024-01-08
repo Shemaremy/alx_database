dd84542c3b64ec6299656f4eea78ce597b3b6290
Shemaremy <remyshema20@gmail.com>
2024-01-08 20:23:48 +0200

    commit 342

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,13 @@
+-- Create the database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS cities (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
