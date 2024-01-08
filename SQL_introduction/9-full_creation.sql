7e4429ae32465a698bc085c84a9a835718215154
Shemaremy <remyshema20@gmail.com>
2024-01-08 15:36:48 +0200

    commit 382

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -3,12 +3,9 @@ DROP DATABASE IF EXISTS hbtn_test_db_9;
 CREATE DATABASE IF NOT EXISTS hbtn_test_db_9;
 USE hbtn_test_db_9;
 CREATE table IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table VALUES (
-    id = 1, name = “John”, score = 10
-    id = 2, name = “Alex”, score = 3
-    id = 3, name = “Bob”, score = 14
-    id = 4, name = “George”, score = 8
-
-)
+INSERT INTO second_table VALUES (1, “John”, 10)
+INSERT INTO second_table VALUES(2, “Alex”, 3)
+INSERT INTO second_table VALUES(3, “Bob”, 14)
+INSERT INTO second_table VALUES(4, “George”, 8)
 
 
