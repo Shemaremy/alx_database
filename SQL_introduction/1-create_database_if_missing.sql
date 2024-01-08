cc9c35d83fdf1e89ae3d0c1eb169a15549cd71bc
Shemaremy <remyshema20@gmail.com>
2024-01-08 13:38:42 +0200

    commit 302

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,4 @@
--- Deleting a database
+-- Here we are creating a database, if it already exists, we drop it then create it bundi bushyashya
 DROP DATABASE IF EXISTS hbtn_0c_0;
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 
