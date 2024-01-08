1d87771ad53849fbab5c8522555b19bf8673e8ea
Shemaremy <remyshema20@gmail.com>
2024-01-08 14:50:09 +0200

    commit 223

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,9 @@
 -- A script that prints the full description of the table first_table
-SHOW TABLE first_table;
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128),
+    c CHAR(1),
+    created_at DATE,
+    PRIMARY KEY (id)
+);
+DESC first_table;
