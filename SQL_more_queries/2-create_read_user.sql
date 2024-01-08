e04c5e1f005c7bfab3a0415c1e7fbca0cca46de6
Shemaremy <remyshema20@gmail.com>
2024-01-08 17:26:53 +0200

    commit 392

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,6 @@
+-- Creating the database called 'hbtn_0d_2'
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+
