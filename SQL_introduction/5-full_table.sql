d387dc0bfeb56eb9195409e200edbd231aa59f35
Shemaremy <remyshema20@gmail.com>
2024-01-08 14:24:12 +0200

    commit 201

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,11 @@
 -- A script that prints the full description of the table first_table
-DESC first_table;
+DROP DATABASE IF EXISTS hbtn_test_db_5;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
+USE hbtn_test_db_5;
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(128),
+    c CHAR(1),
+    created_at DATE,
+    PRIMARY KEY (id)
+);
