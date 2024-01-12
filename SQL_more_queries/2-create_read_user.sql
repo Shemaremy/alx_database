9f4bd3c2024e57381d3d692cf98a84bb1fa98576
Remy <remyshema20@gmail.com>
2024-01-12 09:53:16 +0200

    Update 2-create_read_user.sql

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,9 @@
 -- Creating the database called 'hbtn_0d_2'
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
+-- Normal way to create a user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- Granting select on Database hbtn on all tables to user .......
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 
